a2bfb31c4e24daa27b268dc9d11b98c239b5e273
uchennaanya <uchennaongit@gmail.com>
2020-08-21 15:12:06 +0100

    component part 1

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap'
+import Menu from './components/MenuComponent';
 function App() {
   return (
-    <div className="App">
+    <div>
       <Navbar dark color="primary">
         <div className="container">
           <NavbarBrand href="/">Con Fusion </NavbarBrand>
         </div>
       </Navbar>
+      <Menu />
     </div>
   );
 }
